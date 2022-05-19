06c34da88396e65c7bd9adec4e30ab4eb97c348c
naveenguttedar <naveenguttedar844@gmail.com>
2022-05-19 19:31:56 +0530

    added git-hub dependencies

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
-import React from "react"
-import ReactDOM from "react-dom"
-
-import App from './App'
-ReactDOM.render(<App />, document.getElementById("root"))
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+/**
+ * Challenge: Add an event listener to the button so when
+ * it is clicked, it adds another thing to our array
+ *
+ * Hint: use the array length + 1 to determine the number
+ * of the "Thing" being added. Also, have you event listener
+ * console.log(thingsArray) after adding the new item to the
+ * array
+ *
+ * Spoiler: the page won't update when new things get added
+ * to the array!
+ */
+function App() {
+  let thingsArray = ["Thing 1", "Thing 2"];
+  let thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
+  let [result, setResult] = React.useState(thingsElements);
+  function add() {
+    setResult((prevElement) => [
+      ...prevElement,
+      <p>Thing {prevElement.length + 1}</p>,
+    ]);
+  }
+  return (
+    <div>
+      <button onClick={add}>Add Item</button>
+      {result}
+    </div>
+  );
+}
+root = createRoot(document.getElementById("root"));
+root.render(<App />);
